e6731f1005f30f2f949f77c7baa9f49f886b1d5a
Haddee Ann <haddeeann@gmail.com>
2022-01-07 19:35:45 -0500

    dummy dropdown

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import Page from "../../components/Page";
+import DropdownSelect from '../../components/DropdownSelect';
 
 export default function Home(){
     return <Page headerTitle={"Home"}>
         <>
+        <DropdownSelect
+            options={[{ label: 'here', value: 'now' }]}
+            placeholder='stay here now'
+            onSelectOption={() => console.log('hi')}
+            value={{ label: 'there', value: 'somewhere'}}
+        />
          Implement Project Here
         </>
     </Page>
