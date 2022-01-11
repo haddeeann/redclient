0f9da596509becbfbafe44fe274e206a44209d14
Haddee Ann <haddeeann@gmail.com>
2022-01-11 12:48:42 -0500

    fixed an ability to delete orders

--- src/api/deleteOrder.ts
+++ src/api/deleteOrder.ts
@@ -1,11 +1,15 @@
 const key : string = process.env.REACT_APP_API_KEY || "";
 const deleteOrder = async (idArr: number[]) => {
+    console.log(idArr);
     await fetch("https://red-candidate-web.azurewebsites.net/api/Orders/delete", {
-        method: "DELETE",
+        method: "POST",
         headers: {
-            "ApiKey": key
+            "ApiKey": key,
+            "Content-Type": "application/json"
         },
         body: JSON.stringify(idArr)
+    }).then(result => {
+        // console.log(result);
     }).catch(error => {
         console.log(error);
     });
